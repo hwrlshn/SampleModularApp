de02cf5e40f46a69512dc300a7b27ff8dcb30b9e
Grafonsky <d3cembry@gmail.com>
2024-05-10 20:38:47 +0600

    Init project

---
+++ SampleModularApp/SampleModularApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleModularApp
+//
+//  Created by Bohdan Hawrylyshyn on 10.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
